beacaaf1f8ed1dcfcdafda22cb7e043fa3fcd9b2
amiiitsaha <127011427+amiiitsaha@users.noreply.github.com>
2024-02-24 17:02:44 +0530

    Create dd.php
    changes

---
+++ dd.php
@@ -0,0 +1,24 @@
+<?php
+
+
+// if(isset($_GET['submit'])){
+
+// $to = "amiiitsaha@gmail.com";
+// $subject = "Test Mail";
+// $message = "This is a test email message.";
+
+// // Additional headers
+// $headers = "From: azizhaziq2@gmail.com\r\n";
+// $headers .= "To: amiiitsaha@gmail.com\r\n";
+
+
+// // Mail function
+// $mail_success = mail($to, $subject, $message, $headers);
+
+// if ($mail_success) {
+//     echo "Mail sent successfully!";
+// } else {
+//     echo "Error sending mail.";
+// }
+echo "hello world";
+?>
